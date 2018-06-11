68381214357d3752865b090bc5a24c25f7401703
Alexei <A@Alexeis-MacBook-Pro-2.local>
2018-06-11 20:00:05 +0300

    Angular app template

--- public/src/Customer/CustomerCtrl.js
+++ public/src/Customer/CustomerCtrl.js
@@ -4,11 +4,13 @@ droneCafe.controller('CustomerCtrl', function($scope, CustomerService) {
   $scope.dishes = [];
 
   CustomerService.getOrders().then(function(res) {
+    console.log(res.data);
     $scope.orders = res.data;
   });
 
 
   CustomerService.getDishes().then(function(res) {
+    console.log(res.data);
     $scope.dishes = res.data;
   });
 
--- public/src/Kitchen/KitchenCtrl.js
+++ public/src/Kitchen/KitchenCtrl.js
@@ -4,10 +4,12 @@ droneCafe.controller('CustomerCtrl', function($scope, KitchenService) {
   $scope.cookingDishes = [];
 
   KitchenService.getOrders('ordered').then(function(res) {
+    console.log(res.data);
     $scope.orderedDishes = res.data;
   });
 
   KitchenService.getOrders('cooking').then(function(res) {
+    console.log(res.data);
     $scope.cookingDishes = res.data;
   });
 
